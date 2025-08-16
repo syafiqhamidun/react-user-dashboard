bba6315a9e23a867b3ea34ab2b2cb91324b2879b
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2025-08-16 14:47:17 +0800

    chore: initialize project with Vite + React + TypeScript + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-12">
+      <header className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-3">User Directory</h1>
+        <p className="text-gray-800">Easily explore and filter user profiles from our directory.</p>
+        <hr className="border-b-2"/>
+      </header>
+
+    </div>
+  )
+}
+
+export default App
